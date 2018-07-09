e397fd1a99816599a7ee1eb28ff7e81c1a232fa3
platin91 <s9foschw@stud.uni-saarland.de>
2018-07-10 00:11:23 +0200

    new iteration count for reasons

--- test.mathexp.sh
+++ test.mathexp.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 start=0$1
-(for i in `seq $start 0`; do echo $i --------; env BFS=true R=$i P=U python3 generate_reject.py ../pychains/subjects/mathexpr.py 10; done ) 2>mathexpr.err | tee mathexpr.log
+(for i in `seq $start 0`; do echo $i --------; env BFS=true R=$i P=U python3 generate_reject.py ../pychains/subjects/mathexpr.py 100; done ) 2>mathexpr.err | tee mathexpr.log
 
 trap times EXIT
