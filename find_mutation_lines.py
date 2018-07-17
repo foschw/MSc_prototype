0ecd29e1be1e6b4ac2ee9c1d7a6b34b3fb301f6d
platin91 <s9foschw@stud.uni-saarland.de>
2018-07-17 23:58:22 +0200

    changed from regex to ask compiler

--- find_mutation_lines.py
+++ find_mutation_lines.py
@@ -7,7 +7,32 @@ import re
 import ArgTracer
 
 RE_if = re.compile(r'^\s*(if|elif)\s+([^:]|(:[^\s]))+:\s.*')
-RE_cond = re.compile(r'^\s*(if|elif)\s+([^:]|(:[^\s]))+:\s')
+
+def extract_from_condition(cond):
+    poss = cond.count(":")
+    if poss < 1:
+        raise ValueError(cond, "is is not a valid condition statement!")
+    elif poss == 1:
+        try:
+            eval(cond[cond.find("if")+2:cond.find(":")])
+        except SyntaxError:
+            raise ValueError(cond, "is not executable in python")
+        except:
+            return cond[cond.find("if")+2:cond.find(":")]
+    else:
+        idx = 0
+        lastvalid = ""
+        while poss > 0:
+            idx = cond.find(":", idx+1)
+            poss -= 1
+            substr = cond[cond.find("if")+2:idx]
+            try:
+                eval(substr)
+            except SyntaxError:
+                continue
+            except:
+                lastvalid = substr
+        return lastvalid
 
 if __name__ == "__main__":
     arg = sys.argv[1]
@@ -29,6 +54,6 @@ if __name__ == "__main__":
         for i, line in enumerate(fp):
             if i+1 in lines and (RE_if.findall(line)):
                 options[i+1] = (line, 2)
-                print(RE_cond.match(line).group())
+                print(extract_from_condition(line))
 
     print(options)
