3dd2f360b6635d45ba536916e89b1f5fb902a689
s9foschw <s9foschw@stud.uni-saarland.de>
2018-09-10 20:37:06 +0200

    Changed argument order (since number of iterations is basically never required)

--- py_mutate.py
+++ py_mutate.py
@@ -6,19 +6,22 @@ from find_mutation_lines import main as mutate
 from check_results import main as check
 
 def main(argv):
-    # The argument order is: program path, number of pychains iterations, binary in- and output file
+    # The argument order is: program path, binary input file, number of pychains iterations
     if len(argv) < 2:
         raise SystemExit("Please specifiy a .py file as argument.")
     prog = argv[1] if argv[1].endswith(".py") else argv[1] + ".py"
-    iterations = 1000 if len(argv) < 3 else argv[2]
-    binfile = "rejected_" + prog[prog.rfind("/")+1:prog.rfind(".py")] + ".bin" if len(argv) < 4 else argv[3]
-    print("Generating input for:", prog, "...")
-    gen([None, prog, iterations, binfile])
+    binfile = "rejected_" + prog[prog.rfind("/")+1:prog.rfind(".py")] + ".bin" if len(argv) < 3 else argv[2]
+    iterations = 1000 if len(argv) < 4 else argv[3]
+    if len(argv) < 3:
+        print("Generating input for:", prog, "...")
+        gen([None, prog, iterations, binfile])
+    else:
+        print("Using inputs from:", binfile)
     print("Starting mutation...", prog)
     mutate([None, prog, binfile])
     print("Testing result integrity...")
     check([None, prog, binfile])
 
 if __name__ == "__main__":
-    print("The argument order is: program path [, number of pychains iterations, binary in- and output file]")
+    print("The argument order is: program path [, binary input file, number of pychains iterations]")
     main(sys.argv)
