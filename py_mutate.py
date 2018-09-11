c75f08227bfb7246a640f711e0cc00576268a36d
s9foschw <s9foschw@stud.uni-saarland.de>
2018-09-11 16:04:17 +0200

    Changed from argument order to opts (-b and -i)

--- py_mutate.py
+++ py_mutate.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import getopt
 sys.path.append('.')
 from generate_reject import main as gen
 from find_mutation_lines import main as mutate
@@ -7,11 +8,9 @@ from check_results import main as check
 
 def main(argv):
     # The argument order is: program path, binary input file, number of pychains iterations
-    if len(argv) < 2:
-        raise SystemExit("Please specifiy a .py file as argument.")
     prog = argv[1] if argv[1].endswith(".py") else argv[1] + ".py"
-    binfile = "rejected_" + prog[prog.rfind("/")+1:prog.rfind(".py")] + ".bin" if len(argv) < 3 else argv[2]
-    iterations = 1000 if len(argv) < 4 else argv[3]
+    binfile = "rejected_" + prog[prog.rfind("/")+1:prog.rfind(".py")] + ".bin" if not argv[2] else argv[2]
+    iterations = 1000 if not argv[3] else argv[3]
     if len(argv) < 3:
         print("Generating input for:", prog, "...")
         gen([None, prog, iterations, binfile])
@@ -23,5 +22,20 @@ def main(argv):
     check([None, prog, binfile])
 
 if __name__ == "__main__":
-    print("The argument order is: program path [, binary input file, number of pychains iterations]")
-    main(sys.argv)
+    print('The arguments are: "program path" [, -b "binary input file", -i "number of pychains iterations"]', flush=True)
+    
+    binfile = None
+    iterations = None
+
+    if len(sys.argv) < 2:
+        raise SystemExit("Please specifiy a .py file as argument.")
+
+    opts, args = getopt.getopt(sys.argv[2:], "b:i:")
+    for opt, a in opts:
+        if opt == "-b":
+            print("Using binary file:", a, flush=True)
+            binfile = a
+        elif opt == "-i":
+            iterations = int(a)
+
+    main([sys.argv[0],sys.argv[1],binfile,iterations])
