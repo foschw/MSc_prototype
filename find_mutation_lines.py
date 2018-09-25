bdef9248900a23d5665cdf320b089864f65156f9
s9foschw <s9foschw@stud.uni-saarland.de>
2018-09-25 17:19:28 +0200

    Added comments

--- find_mutation_lines.py
+++ find_mutation_lines.py
@@ -11,6 +11,10 @@ import os
 import glob
 import imp
 
+# Computes the "left difference" of two sets.
+# This returns two sets: 
+# prim(ary): the list of conditions that have a fixed value for the valid string which differs from the mutated string
+# sec(ondary): the list of condition that the mutated string caused but were not relevant to the original string (either True and False or not seen at all).
 def get_left_diff(d1, d2):
     prim = []
     sec = []
@@ -28,13 +32,22 @@ def get_left_diff(d1, d2):
 
     return (prim, sec)
 
+# Sanitizes an input string. Introduced for ease of modification.
 def sanitize(valuestring):
     return repr(valuestring)
 
+# Creates all possible conditions that may adjust the path of the mutated string to be like the original string
+# Parameters:
+# old_cond: The pair (old condition line number, truth value)
+# file: The current mutants file path
+# b_varsat: The variable assignments of the original string (base string)
+# varsat: The variable assignments encountered for the mutated string
 def make_new_conditions(old_cond, file, b_varsat, varsat):
     global manual_errs
     (lineno, state) = old_cond
+    # Get the full condition string
     full_str = manual_errs.get_if_from_line(lineno, file)
+    # Reduce the string to only the condition, i.e. "if True:" becomes "True"
     cond_str = full_str[full_str.find("if")+3:full_str.rfind(":")]
     # Conditions that hold for the last execution of the line and not for the baseline run
     lcand = varsat.get(lineno) if varsat.get(lineno) else None
@@ -48,6 +61,7 @@ def make_new_conditions(old_cond, file, b_varsat, varsat):
         return []
 
     possible_conditions = []
+    # Generate all conditions that may flip the truth value
     for valid_cond in choices:
         if state:
             # Falsify the condition
@@ -65,21 +79,26 @@ def make_new_conditions(old_cond, file, b_varsat, varsat):
 
     return possible_conditions
 
+# Generate all possible new conditions from a delta (pair of primary and secondary condition differences).
 def get_possible_fixes(delta, file, b_varsat, varsat):
     (prim, sec) = delta
     fixmap = []
+    # In case there are primary candidates generate a mutant for all of them.
     if prim:
         for (lineno, state) in prim:
         	fix_list = make_new_conditions((lineno,state),file,b_varsat,varsat)
         	if fix_list:
         	    fixmap.append((fix_list, lineno))
+    # Otherwise test all remaining candidates
     elif sec:
         for (lineno, state) in sec:
         	fix_list = make_new_conditions((lineno,state),file,b_varsat,varsat)
         	if fix_list:
         	    fixmap.append((fix_list, lineno))
+    # Returns a list of ([possible fixes for a line], line number)
     return fixmap
 
+# Creates a mutant by copying the script it is derived from and applying modifications for given lines
 def file_copy_replace(target, source, modifications):
     with open(target, "w", encoding="UTF-8") as trgt:
         with open(source, "r", encoding="UTF-8") as src:
@@ -89,6 +108,8 @@ def file_copy_replace(target, source, modifications):
                 elif modifications.get(i+1) != "":
                     trgt.write(line)
 
+# Removes all mutants that are intermediate (i.e. we cannot tell whether they are equivalent)
+# The argument completed stores all non-equivalent mutants
 def cleanup(mut_dir, completed):
     for fl in glob.glob(mut_dir + "/*.py"):
         fl = fl.replace("\\", "/")
@@ -102,7 +123,7 @@ def main(argv):
     mut_dir = arg[arg.rfind("/")+1:arg.rfind(".")] if arg.rfind("/") >= 0 else arg[:arg.rfind(".")]
     script_name = mut_dir
     mut_dir = "mutants/" + mut_dir + "/"
-    # Store the reason why the mutantion was completed
+    # Store the reason why the mutation was completed
     mutants_with_cause = []
     # Timeout since our modifications may cause infinite loops
     timeout = 5
@@ -138,6 +159,7 @@ def main(argv):
     	raise SystemExit("Exiting: " + pick_file + " contains no valid inputs for " + ar1)
     print("Used baseinput:", repr(basein))
 
+    # Run the mutation process for every rejected string
     for s in rej_strs:
         s = s[0]
         queue = [(ar1, [])]
@@ -168,7 +190,6 @@ def main(argv):
             	print("Tracer timed out on mutated string")
             	discarded.add(arg)
             	continue
-            # Moved this condition after the second call to make missing infinite loops less likely
             if berr:
                 print("Mutation complete:", arg, "(base rejected)")
                 mutants_with_cause.append((arg, "valid string rejected"))
@@ -189,10 +210,12 @@ def main(argv):
             print("Mutated string rejected:", err, "manually raised:", manual)
             if err and manual:
                 if not berr:
+                    # In case the base string is not rejected we discard the script, otherwise we can keep it
                 	discarded.add(arg)
                 all_fixes = get_possible_fixes((prim, sec), arg, b_vrs, vrs)
                 if all_fixes:
                     for (fix_list, fix_line) in all_fixes:
+                        # Create a mutant for every possible fix
                         for fix in fix_list:
                             if not fix.endswith("\n"):
                                 fix = fix + "\n"
@@ -201,19 +224,25 @@ def main(argv):
                             queue.append((cand, history.copy()+[fix_line]))
                             file_copy_replace(cand, arg, mods)
                             mut_cnt += 1
+            # Stop the mutation when the originally rejected string is accepted
             elif arg != ar1:
             	print("Mutation complete:", arg, "(mutated string accepted)")
             	mutants_with_cause.append((arg, "mutated string accepted"))
             		
+        # Don't delete the original script, we need it to create mutants from whenever a new rejected string is processed
         discarded.discard(ar1)
+        # Remove all scripts that neither reject the base string nor accept the mutated string
         for scrpt in discarded:
             print("Removed:", scrpt)
             os.remove(scrpt)
+        # Adjust the file naming
         str_cnt += 1
         mut_cnt = 0
         print("Processing string number:", str(str_cnt), "/", str(len(rej_strs)))
+    # Remove the copy of the original script since it is not a mutant
     os.remove(ar1)
     print("Done. The final mutants are in:", mut_dir)
+    # Write a log on why each file was kept. This way we (or check_results.py) can check whether the mutation procedure is working correctly.
     with open(mut_dir[:-1] + "_inputs.log", "w", encoding="UTF-8") as file:
             for i in range(len(rej_strs)):
                 file.write(str(i) + ": " + repr(rej_strs[i][0])+"\n")
