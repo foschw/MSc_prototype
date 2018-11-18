919272713d915043224f4daf409fe68fc2276d29
s9foschw <s9foschw@stud.uni-saarland.de>
2018-11-19 00:56:28 +0100

    Added test suite for xsum

--- subjects/artificial/xsum.py
+++ subjects/artificial/xsum.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
 import sys
+import unittest
+
+class TestXSum(unittest.TestCase):
+	def test_valid_1(self):
+		self.assertEqual(main("123"),6)
+
+	def test_valid_2(self):
+		self.assertEqual(main("99"),18)
+
+	def test_invalid_1(self):
+		with self.assertRaises(Exception):
+			main("-1")
+
+	def test_invalid_2(self):
+		with self.assertRaises(Exception):
+			main("34a")
+
 
 def main(val):
     sum = 0
