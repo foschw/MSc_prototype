0c674cb697b71d0b2b3c474437c0900bccaccea3
s9foschw <s9foschw@stud.uni-saarland.de>
2019-04-03 06:21:38 +0200

    Now instantly deletes duplicate files. This makes runtime better in case a lot of duplicates can be found.

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -10,7 +10,7 @@ def read_file_hashed(filename):
         res = fli.read()
         return (res,hash(res))
 
-# Simple duplicate removal method - given a file directory (mutation results), a file extension (.py) as well as the list of mutant, cause pairs
+# Simple duplicate removal method - given a file directory (mutation results), a file extension (.py) as well as the list of (mutant, cause) pairs
 # Removes all duplicates (i.e. same content) from both the file-system and the list of pairs
 def remove_duplicates(fdir, ext, pairlst):
     print("Removing duplicates...", flush=True)
@@ -18,44 +18,45 @@ def remove_duplicates(fdir, ext, pairlst):
     fdir = fdir if not fdir.endswith("/") else fdir[:-1]
     files = []
     dups = []
-    rmdup = []
     for fl in glob.glob(fdir + "/*." + ext):
         fl = fl.replace("\\", "/")
         files.append(fl)
 
     if len(files) < 2:
         return pairlst
-    cmps = (len(files)*(len(files)-1))/2
     lstep = 0
     prog = 0
+    idx1 = 0
 
-    for idx1 in range(len(files)):
+    while idx1 < len(files):
         fl1 = files[idx1]
+        idx2 = idx1 +1
+        idx1 += 1
         s1 = read_file_hashed(fl1)
-        for idx2 in range(idx1+1,len(files)):
+        while idx2 < len(files):
+       	    cmps = (len(files)*(len(files)-1))/2
             fl2 = files[idx2]
             s2 = read_file_hashed(fl2)
             if s1[1] == s2[1] and s1[0] == s2[0]:
-                dups.append((fl1, fl2))
+                dups.append(fl2)
+                os.remove(fl2)
+                del files[idx2]
+            else:
+            	idx2 += 1
             prog += 1
             cprog = (prog/cmps)*100
             if cprog > lstep:
                 lstep += 1
-                print("Progress:", int(cprog), "%", flush=True)
-
-    for (a, b) in dups:
-        if os.path.exists(a) and os.path.exists(b):
-            os.remove(b)
-            rmdup.append(b)
+                print("Progress:", min(int(cprog),100), "%", flush=True)
 
     i = 0
     while i < len(pairlst):
-        if pairlst[i][0] in rmdup:
+        if pairlst[i][0] in dups:
             del pairlst[i]
         else:
             i += 1
 
-    print("Removed duplicates:", len(rmdup), flush=True)
+    print("Removed duplicates:", len(dups), flush=True)
 
     return pairlst
 
@@ -69,6 +70,7 @@ def main(logfile, folder):
 				mutants_with_cause.append(eval(ln))
 
 	mutants_with_cause = remove_duplicates(folder, ".py", mutants_with_cause)
+
 	with open(logfile, "w", encoding="UTF-8") as f:
 		f.write(first_line)
 		for e in mutants_with_cause:
