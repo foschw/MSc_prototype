89dde2761cdadf4002f4ff0034ea6368e28f7a89
s9foschw <s9foschw@stud.uni-saarland.de>
2019-09-03 22:42:14 +0200

    Now also outputs a file containing which test cases a mutant failed.

--- run_unittests.py
+++ run_unittests.py
@@ -24,34 +24,41 @@ def run_unittests_for_script(script,tmout=None):
 	except subprocess.TimeoutExpired:
 		proc.kill()
 		proc.communicate()
-		return (-1,-1)
+		return (-1,None)
 	except:
 		raise SystemExit("Unit test execution failed.")
-	return extract_test_stats(res)
+	return extract_test_stats(res, script)
 
-# Extracts the amount of test passes p and fails f as pair (p, f)
-def extract_test_stats(unittest_output):
-	outpt_lines = reversed(unittest_output.lstrip().rstrip().split("\n"))
+# Extracts the total amount of test cases as well as the identifiers of the failed tests
+def extract_test_stats(unittest_output, script):
+	script = script if script.rfind("/") == -1 else script[script.rfind("/")+1:]
+	if script.endswith(".py"):
+		script = script[:-3]
+	outpt_lines = unittest_output.lstrip().rstrip().split("\n") # reversed()
 	re_test_num = r"^Ran \d+ test(s)? in \d(\.)?\d*s"
-	re_test_fails = r"^FAILED \(((failures(=\d+)?)?(, )?(errors)?)=\d+\)"
+	re_tstname = r"^=+$"
 	total_tests = 0
-	num_fail = -2
-	for l in outpt_lines:
-		l = l.lstrip().rstrip()
-		if re.match(re_test_num, l):
-			total_tests = int(l[4:l.find("test")])
-			break
-		elif (l == "OK"):
-			num_fail = 0
-		elif re.match(re_test_fails,l):
-			re_num = re.compile("=\d+")
-			num_fail = 0
-			for ob in re_num.findall(l):
-				num_fail += int(ob[1:])
-	# This requires manual examination
-	if num_fail == -2:
-		return (-2,-2)
-	return (total_tests-num_fail,num_fail)
+	nxt = False
+	ftest_names = set()
+	for ol in outpt_lines:
+		ol = ol.lstrip().rstrip()
+		if nxt:
+			ftest_names.add(generify_name(ol, script))
+		if re.match(re_tstname, ol):
+			nxt = True
+		else:
+			nxt = False
+
+		if re.match(re_test_num, ol):
+			total_tests = int(ol[4:ol.find("test")])
+
+	return (total_tests, ftest_names)
+
+def generify_name(line, script):
+	line = line.lstrip().rstrip()
+	res = line[line.find(":")+2:]
+	res = res.replace("(" + script + ".", "(")
+	return res
 
 # Running tests can be done independently
 def run_tests_threaded(script):
@@ -66,10 +73,12 @@ def main(argv):
 	argv[1] = argv[1].replace("\\", "/")
 	argv[1] = argv[1][:-1] if argv[1].endswith("/") else argv[1]
 	test_res_fl = argv[1] + "_test_results.log"
+	mutant_fail_dict = argv[1] + "_fail_dict.log"
 	lwriter = LogWriter(test_res_fl)
 	scripts_f = []
 	scripts_p = []
 	targets = []
+	mutant_to_testfail = {}
 	num_workers = int(current_config["test_threads"])
 
 	for fnm in glob.iglob(argv[1]+"/*.py", recursive=True):
@@ -83,7 +92,16 @@ def main(argv):
 		for future in concurrent.futures.as_completed(future_to_script):
 			print("Running tests:", str(fidx+1) + "/" + str(len(targets)), flush=True)
 			test_script = future_to_script[future]
-			(tpass, tfail) = future.result()
+			(testnum, failnames) = future.result()
+			if failnames is not None:
+				tpass = testnum-len(failnames)
+				tfail = len(failnames)
+				mutant_to_testfail[test_script] = failnames
+
+			else:
+				tpass = -1
+				tfail = -1
+
 			if tfail == 0:
 				if tpass > 0:
 					scripts_p.append((test_script,(tpass,tfail)))
@@ -108,6 +126,10 @@ def main(argv):
 		for (scrpt, (tpass,tfail)) in scripts_f:
 			dest.write(scrpt + ":\nPass: " + str(tpass) + ", Fail: " + str(tfail) + " \n")
 			dest.write("\n")
+
+	# Log which tests the mutants failed to enable minimal mutant set generation
+	with open(mutant_fail_dict, "w", encoding="UTF-8") as dest:
+		dest.write(repr(mutant_to_testfail))
 
 def by_fail(result):
 	(_, (_, tfail)) = result
